676304f4539e9144d092934cf0bfb4c97523602c
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-16 17:16:26 +0000

    add show and collapase icon for sidebar menu

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -5,9 +5,7 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/80 backdrop-blur-sm">
       <div className="container flex h-14 items-center px-4 lg:px-6">
-        <div className="md:hidden">
-          <SidebarTrigger />
-        </div>
+        <SidebarTrigger />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <UserNav />
         </div>
